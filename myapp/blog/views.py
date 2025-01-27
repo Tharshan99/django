713636cc2a5dbec7c27ad9a42a6752b09ca53f6e
Tharshan99 <tharshanthamizlan@gmail.com>
2025-01-27 13:40:02 +0530

    Dynamic Post

--- myapp/blog/views.py
+++ myapp/blog/views.py
@@ -5,7 +5,13 @@ from django.urls import reverse
 # Create your views here.
 def index(request):
     blog_title = "My Blog"
-    return render(request, "blog/index.html", {"blog_titles": blog_title})
+    posts = [
+        {'title' : 'Post 1', 'Content': 'Content of Post 1'},
+        {'title' : 'Post 2', 'Content': 'Content of Post 2'},
+        {'title' : 'Post 3', 'Content': 'Content of Post 3'},
+        {'title' : 'Post 4', 'Content': 'Content of Post 4'},
+    ]
+    return render(request, "blog/index.html", {"blog_titles": blog_title, "postss" : posts})
 
 def detail(request):
     return render(request, "blog/detail.html")
