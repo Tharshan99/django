5a982adeda14acdc12618fce8d14763b5493fd5b
Tharshan99 <tharshanthamizlan@gmail.com>
2025-01-28 12:59:33 +0530

    Getting Post Data

--- myapp/blog/views.py
+++ myapp/blog/views.py
@@ -2,18 +2,21 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.urls import reverse
 import logging
+from blog.models import Post
 
 # Create your views here.
 
-posts = [
-        {'id':1, 'title' : 'Post 1', 'Content': 'Content of Post 1'},
-        {'id':2, 'title' : 'Post 2', 'Content': 'Content of Post 2'},
-        {'id':3, 'title' : 'Post 3', 'Content': 'Content of Post 3'},
-        {'id':4, 'title' : 'Post 4', 'Content': 'Content of Post 4'},
-    ]
+# posts = [
+#         {'id':1, 'title' : 'Post 1', 'Content': 'Content of Post 1'},
+#         {'id':2, 'title' : 'Post 2', 'Content': 'Content of Post 2'},
+#         {'id':3, 'title' : 'Post 3', 'Content': 'Content of Post 3'},
+#         {'id':4, 'title' : 'Post 4', 'Content': 'Content of Post 4'},
+#     ]
 
 def index(request):
     blog_title = "My Blog"
+    # getting data from post model
+    posts = Post.objects.all()
     return render(request, "blog/index.html", {"blog_titles": blog_title, "postss" : posts})
 
 def detail(request, post_id):
